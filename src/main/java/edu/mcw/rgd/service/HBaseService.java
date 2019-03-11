30d80adf1a1d564f3504154f2eb1d05729437c95
jt15 <jthota01@gmail.com>
2019-03-11 13:29:10 -0500

    added properties file

--- src/main/java/edu/mcw/rgd/service/HBaseService.java
+++ src/main/java/edu/mcw/rgd/service/HBaseService.java
@@ -1,10 +1,12 @@
 package edu.mcw.rgd.service;
 
+import java.io.FileInputStream;
 import java.net.URI;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 import java.util.Calendar;
+import java.util.Properties;
 
 
 import edu.mcw.rgd.Utils.BasicUtils;
@@ -24,13 +26,28 @@ public class HBaseService {
 	/*private static final Logger logger = Logger
 			.getLogger(QueryFormController.class);*/
 
-	private static String HBASE_REST_SERVICE_HOST= "gray04.rgd.mcw.edu:8080";
-	
+
+	private static String HBASE_REST_SERVICE_HOST= getHbaseServiceProperties().getProperty("HBASE_REST_SERVICE_HOST");
+	private static String HBASE_TABLE=getHbaseServiceProperties().getProperty("HBASE_TABLE");
+
+
+	public static Properties getHbaseServiceProperties(){
+		Properties props= new Properties();
+		FileInputStream fis=null;
+		try{
+			fis=new FileInputStream("~/properties/hbaseService.properties");
+			props.load(fis);
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		return props;
+	}
+
 	public static String getHbaseField(String Pmid, String column) {
 		try {
 			String hbaseKey = new StringBuilder(Pmid).reverse().toString();
 
-			URI uri = new URI("http",HBASE_REST_SERVICE_HOST, "/pubmed18/"+hbaseKey+"/"+column, null, null);
+			URI uri = new URI("http",HBASE_REST_SERVICE_HOST, "/"+HBASE_TABLE+"/"+hbaseKey+"/"+column, null, null);
 			String hbaseQueryStr = uri.toASCIIString(); 
 			String value = BasicUtils.restGet(hbaseQueryStr, null);
 			return value;
@@ -44,7 +61,7 @@ public class HBaseService {
 	public static void setHbaseField(String Pmid, String column, String value) throws Exception {
 		try {
 			String hbaseKey = new StringBuilder(Pmid).reverse().toString();
-			URI uri = new URI("http",HBASE_REST_SERVICE_HOST, "/pubmed18/"+hbaseKey+"/"+column, null, null);
+			URI uri = new URI("http",HBASE_REST_SERVICE_HOST, "/"+HBASE_TABLE+"/"+hbaseKey+"/"+column, null, null);
 			String xmlData = "<CellSet><Row key=\"" + Bytes.toString(Base64.encodeBase64(Bytes.toBytes(hbaseKey))) + "\"><Cell column=\""+Bytes.toString(Base64.encodeBase64(Bytes.toBytes(column)))+"\">" + Bytes.toString(Base64.encodeBase64(Bytes.toBytes(value)))+"</Cell></Row></CellSet>";
 
 			String hbaseQueryStr = uri.toASCIIString();
